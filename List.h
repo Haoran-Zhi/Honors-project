c51072639832618ab9c1da8ad5f2906ceb06572d
Haoran-Zhi <56976639+Haoran-Zhi@users.noreply.github.com>
2019-11-07 21:35:31 -0800

    Add files via upload

--- List.h
+++ List.h
@@ -7,7 +7,6 @@ using namespace std;
 
 template<class T>
 class List {
-
 	struct Node
 	{
 		Node(T v, shared_ptr<Node> list) : _value(v), _next(list) {}
@@ -32,56 +31,64 @@ private:
 public:
 	List() : _head(nullptr) {}
 	List(T v, List tail) : _head(new Node(v, tail._head)) {}
-	explicit List(Node const* items) : _head(items) {}
+	explicit List(Node* items) : _head(items) {}
 	bool isEmpty() const { return !_head; }
 	//O(1) operations: 
-	List<T> insertFront(T v) const{return new List(v, this); }//prepending
-	List<T> removeFront() const { return new List(_head->_next); }//removing the first element
+	List<T> insertFront(T v){return *new List(v, *this);}//prepending
+	List<T> removeFront() { if (!_head->_next) {return *new List();}return *new List(_head->_next.get()); }//removing the first element
 	T front() const { return _head->_value; }//getting the first element
-
 	//O(n) operations:
-	List<T>& insertEnd(T v) const;
-	List<T>& removeEnd() const;
-
-
-
-
-
-
+	List<T> insertEnd(T v) const;
+	List<T> removeEnd() const;
+	T getTail() const;
+	List<T> InsertAt(int position, T v) const;
+	List<T> RemoveAt(int position, T v) const;
 };
 
+
+
 template<class T>
- List<T>& List<T>::insertEnd(T v) const
+ List<T> List<T>::insertEnd(T v) const
 {	//set up the base case to be when reaching the end of the list
-	 if (!this.isEmpty()) {
-		 //insert the node to the new list
-		 return new List()->insertFront(v);
-	 }
-	 //recursion call
-	 List<T> list = _head->_next.insertEnd(v);
-	 //when going backward, copy the list into the new list
-	 list.insertFront(_head->_value);
-	 //return the new list
-	 return list;
-
-
+	 if (isEmpty()) { return *new List(v, *this);}
+	 List<T> list = *new List(_head->_next.get());
+	 return list.insertEnd(v).insertFront(_head->_value);
 }
 
  template<class T>
-List<T>& List<T>::removeEnd() const
+List<T> List<T>::removeEnd() const
  {
-	if (!this.isEmpty()) {
-		//create a new empty list
-		return new List();
-	}
-	//recursion call
-	List<T> list = _head->_next.removeEnd(v);
-	//when going backward, copy the list into the new list
-	list.insertFront(_head->_value);
-	//return the new list
-	return list;
+	if (_head->_next == nullptr) {return *new List();}
+	List<T> list = *new List(_head->_next.get());
+	return list.removeEnd().insertFront(_head->_value);
  }
 
+
+
+template<class T>
+inline T List<T>::getTail() const
+{
+	if (!_head->_next) {return _head->_value;}
+	List<T> list = *new List(_head->_next.get());
+	return list.getTail();
+}
+
+template<class T>
+inline List<T> List<T>::InsertAt(int position, T v) const
+{
+	if (position == 0) {return this->insertFront(v);}
+	//return the new list
+	List<T> list = *new List(_head->_next.get());
+	return list.InsertAt(position - 1, v).insertFront(_head.get()._value);
+}
+
+template<class T>
+inline List<T> List<T>::RemoveAt(int position, T v) const
+{
+	if (position == 0) {return this->removeFront(v);}
+	List<T> list = *new List(_head->_next.get());
+	return list.RemoveAt(position - 1, v).insertFront(_head.get()._value);
+}
 
 
 #endif//List_H
